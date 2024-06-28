ca346bf4a182cad68a86431f88020c9b47979047
Latte39777 <s1f102400549@iniad.org>
2024-06-29 06:42:05 +0900

    path

--- server/domain/novel/useCase/novelUseCase.ts
+++ server/domain/novel/useCase/novelUseCase.ts
@@ -1,8 +1,13 @@
+import { load } from 'cheerio';
+import * as iconv from 'iconv-lite';
 import { transaction } from 'service/prismaClient';
 
 export const novelUseCase = {
   scrape: (aozoraUrl: string): Promise<string> =>
     transaction('RepeatableRead', async (tx) => {
-      return aozoraUrl;
+      const html = await fetch(aozoraUrl).then((b) => b.arrayBuffer());
+      const $ = load(iconv.decode(Buffer.from(html), 'Shift_JIS'));
+      const body = $('body');
+      return body.text();
     }),
 };
